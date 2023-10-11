ce0c7c370fcae48e90e25e5be2dd11f247dc4593
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-10-11 22:30:24 +0300

    delete addBooksInShoppingList.js and add pagination.js

--- src/shopping.js
+++ src/shopping.js
@@ -1,2 +1,5 @@
-import './js/addBooksInShoppingList';
+import './js/createMarkupShoppingList';
+import './js/createMarkupEmptyPage';
+import './js/getVisibleBooks';
 import './js/deleteBookFromShoppingList';
+import './js/pagination';
